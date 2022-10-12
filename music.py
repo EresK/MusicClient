11737ca970a444db29d85ad98d088639cba406e0
EresK <e.kuular@g.nsu.ru>
2022-10-12 23:37:11 +0700

    add sample with usage yandex_music API

---
+++ music.py
@@ -0,0 +1,66 @@
+from yandex_music import Client
+import os
+
+client = Client()
+client.init()
+
+
+# Sample - Getting genres
+def print_genres():
+    genres = client.genres()
+    for genre in genres:
+        print(genre['title'])
+        for sub_genre in genre['sub_genres']:
+            print('sub:', sub_genre['title'])
+        print()
+
+
+def print_tracks():
+    chart = client.chart('world').chart
+
+    for track_short in chart.tracks:
+        track, chart = track_short.track, track_short.chart
+        print(track['title'], '-', end=' ')
+        for artist in track['artists']:
+            print(artist['name'], end=' ')
+        print()
+
+
+# using rhythmbox-client
+def play_first_chart_track():
+    chart = client.chart('world').chart
+
+    if len(chart.tracks) > 0:
+        first_track = chart.tracks[0].track
+
+        filename = './first_chart_track_YMusic.mp3'
+
+        if not os.path.exists(filename):
+            first_track.download(filename, codec='mp3', bitrate_in_kbps=128)
+            exit_status = os.system(f'rhythmbox-client --play-uri {filename}')
+            print(f'"rhythmbox-client --play-uri" command exit status: {exit_status}')
+
+
+# using rhythmbox-client
+def play_by_track_id():
+    # September Earth, Wind & Fire
+    track_id = 645494
+    tracks = client.tracks([track_id])
+
+    if len(tracks) > 0:
+        track = tracks[0]
+
+        filename = './september_track_YMusic.mp3'
+
+        if not os.path.exists(filename):
+            track.download(filename, codec='mp3', bitrate_in_kbps=128)
+            exit_status = os.system(f'rhythmbox-client --play-uri {filename}')
+            print(f'"rhythmbox-client --play-uri" command exit status: {exit_status}')
+
+
+print_genres()
+print_tracks()
+
+# only if you have rhythmbox-client player
+# play_first_chart_track()
+# play_by_track_id()
